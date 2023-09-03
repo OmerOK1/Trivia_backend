1df33d1a9419d32528b6e3dbcb5f816caa87967d
OmerOK1 <96621576+OmerOK1@users.noreply.github.com>
2023-09-03 12:58:00 +0300

    Now receiving new game requests from client, receiving correct questions from api accordingly. further implementation of api received objects into db and client side required to proceed.

--- trivia/src/main/java/com/omer/trivia/beans/GameRound.java
+++ trivia/src/main/java/com/omer/trivia/beans/GameRound.java
@@ -11,7 +11,7 @@ import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.List;
 
-@Entity
+/*@Entity
 @Table(name = "game_rounds")
 @Data
 @NoArgsConstructor
@@ -22,37 +22,19 @@ public class GameRound {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    /*@ManyToOne
-    @JoinColumn(name = "round_id")*/
+    *//*@ManyToOne
+    @JoinColumn(name = "round_id")*//*
     //private Round round; // TODO: consider keeping as a Unidirectional Relationship from this end (not having GameRounds property in Round), unless you find reasons to access GameRound from Round.
 
     @Column(nullable = false)
-    private int currentQuestionIdx;
+    private int currentQuestionIdx;*/
 
     //private HashMap<Question, Timestamp> presentTimes; // TODO: Figure out what are your needs and find the right timeStamp type. Fix - 'name unintuitive to use-case'. Find Annotations.
 
-    //private List<PlayerResponse> playerResponses;
+    //private List<PlayerResponse> playerResponses;}
 
 
 
 
 
-}
-//TODO: GameRound:
-// Id: String
-// round: Round
-// –
-// currentQuestionIndex: int
-// presentTimes: map(Question, timestamp)
-// –
-// playerResponses: Map((User, Question)=>playerResponse)
-// –
-// startRound() (??? What tasks happen here ???)
-// getNextQuestion()
-// –
-// addPlayerResponse(player, question, answer, presentTime)
-// –
-// getRoundUserScores(player)
-// getRoundScores()
-// –
-// hasMoreQuestions()
+
